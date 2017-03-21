e40c1a9c92c74298427b0ae8414da49dbb1d7db7
zhaohong <clarkkent.z@outlook.com>
2017-03-21 20:42:22 +0800

    cgic
    cgic

--- zhaohong/cgic/cgic207/mycgi.c
+++ zhaohong/cgic/cgic207/mycgi.c
@@ -15,8 +15,9 @@
 #include "cgic.h"
 #include <string.h>
 #include <stdlib.h>
+#include <sqlite3.h>
 
-void HandleSubmit();
+int HandleSubmit();
 void CookieSet();
 void LoadEnvironment();
 void ShowForm();
@@ -24,9 +25,17 @@ void RadioButtons();
 void SaveEnvironment();
 void Name();
 void Address();
+void show();
+
+int do_insert(int *id,char *name,int *score);
+int do_delete(int *id);
+int do_update(int *id,int *score);
+int call_back(void *param, int column, char **value, char **name);
+sqlite3 * db;
 
 int cgiMain() {
 #ifdef DEBUG
+
 	LoadEnvironment();
 #endif
   if (cgiFormSubmitClicked("loadenvironment") == cgiFormSuccess) {
@@ -37,6 +46,7 @@ int cgiMain() {
   fprintf(cgiOut, "<HTML><meta charset=\"UTF-8\"><HEAD>\n");
 	fprintf(cgiOut, "<TITLE>cgic test</TITLE></HEAD>\n");
 	fprintf(cgiOut, "<BODY><H1>cgic 测试</H1>\n");
+	fprintf(cgiOut, "<body background=\"http://i1.piimg.com/1949/186dab395823b0cc.jpg\">");
   if ((cgiFormSubmitClicked("testcgic") == cgiFormSuccess) ||
 		cgiFormSubmitClicked("saveenvironment") == cgiFormSuccess)
 	{
@@ -47,22 +57,144 @@ int cgiMain() {
   fprintf(cgiOut, "</BODY></HTML>\n");
 	return 0;
 }
-void HandleSubmit(){
-  Name();
+int HandleSubmit(){
   RadioButtons();
   Address();
 
-  if (cgiFormSubmitClicked("saveenvironment") == cgiFormSuccess) {
+	int ret;
+	char * errmsg;
+	if(sqlite3_open("./db/my.db",&db) != SQLITE_OK){
+		printf("%s\n",sqlite3_errmsg(db));
+		return -1;
+	}
+
+	if((ret = sqlite3_exec(db,"create table stu(id integer,name vchar(32) not null,\
+	score integer not null)",NULL,NULL,&errmsg)) != SQLITE_OK){
+
+		if(ret != 1){
+			printf("%s\n",errmsg);
+			sqlite3_close(db);
+			return -1;
+		}
+	}
+	Name();
+	show();
+	if (cgiFormSubmitClicked("saveenvironment") == cgiFormSuccess) {
 		SaveEnvironment();
   }
+	sqlite3_close(db);
 }
+
 void Name(){
-  char name[81];
+  char id[81];
+	char name[81];
+	char score[81];
+	char delete[81];
+	char updateid[81];
+	char updates[81];
+	int idt,scoret,deletet;
+	int uid,uscore;
+
+	cgiFormStringNoNewlines("id", id, 81);
 	cgiFormStringNoNewlines("name", name, 81);
+	cgiFormStringNoNewlines("score", score, 81);
+	cgiFormStringNoNewlines("delete", delete, 81);
+	cgiFormStringNoNewlines("updateid", updateid, 81);
+	cgiFormStringNoNewlines("updates", updates, 81);
+
+	idt = atoi(id);
+	scoret = atoi(score);
+	deletet = atoi(delete);
+	uid = atoi(updateid);
+	uscore = atoi(updates);
+
+	fprintf(cgiOut, "ID: ");
+	cgiHtmlEscape(id);
+	fprintf(cgiOut, "<br>\n");
 	fprintf(cgiOut, "Name: ");
 	cgiHtmlEscape(name);
 	fprintf(cgiOut, "<br>\n");
+	fprintf(cgiOut, "Score: ");
+	cgiHtmlEscape(score);
+	fprintf(cgiOut, "<br>\n");
+	fprintf(cgiOut, "Delete ID: ");
+	cgiHtmlEscape(delete);
+	fprintf(cgiOut, "<br>\n");
+	fprintf(cgiOut, "Update ID: ");
+	cgiHtmlEscape(updateid);
+	fprintf(cgiOut, "&nbsp;&nbsp;&nbsp;&nbsp;");
+	fprintf(cgiOut, "Update score: ");
+	cgiHtmlEscape(updates);
+	fprintf(cgiOut, "<br>\n");
+
+	do_insert(&idt,name,&scoret);
+	do_delete(&deletet);
+	do_update(&uid,&uscore);
 }
+
+int do_insert(int *id,char *name,int *score){
+	char sql[1024];
+  char * errmsg;
+
+	sprintf(sql,"insert into stu values(%d,'%s',%d)",*id,name,*score);
+
+	if(sqlite3_exec(db,sql,NULL,NULL,&errmsg) != SQLITE_OK){
+    printf("%s\n",errmsg);
+
+    return -1;
+  }
+}
+
+int do_delete(int *id){
+	char sql[1024];
+	char * errmsg;
+
+	sprintf(sql,"delete from stu where id = %d",*id);
+
+  if(sqlite3_exec(db,sql,NULL,NULL,&errmsg) != SQLITE_OK){
+    printf("%s\n",errmsg) ;
+    return -1;
+  }
+}
+
+int do_update(int *id,int *score){
+	char sql[1024];
+	char * errmsg;
+
+	sprintf(sql,"update stu set score = %d where id = %d",*score,*id);
+
+	if(sqlite3_exec(db,sql,NULL,NULL,&errmsg) != SQLITE_OK){
+    printf("%s\n",errmsg);
+    return -1;
+  }
+}
+
+void show() {
+	if (cgiFormCheckboxSingle("show") == cgiFormSuccess) {
+		char *errmsg;
+		char **result,**temp;
+		int nrow,ncolumn;
+		int i,j;
+		if(sqlite3_get_table(db,"select * from stu",&result,&nrow,&ncolumn,&errmsg) != SQLITE_OK){
+			printf("%s\n",errmsg);
+		}
+		else{
+			temp = result;
+			fprintf(cgiOut, "数据库表格数据：");
+			printf("<table border=\"15\" bgcolor=\"red\">");
+			for(i = 0;i < nrow + 1;i++){
+				printf("<tr>");
+				for(j = 0;j < ncolumn;j++){
+					printf("<td>%15s</td>",*(temp++));
+				}
+        printf("</tr>");
+			}
+			printf("</table>");
+		}
+		sqlite3_free_table(result);
+  }
+}
+
 void Address() {
 	char address[241];
   char add[241];
@@ -89,9 +221,9 @@ void RadioButtons(){
   int i,j,k;
   float n,m;
 	cgiFormRadio("age", ages, 4, &ageChoice, 0);
-	/*fprintf(cgiOut, "Age of Truck: %s (method #1)<BR>\n",
-		ages[ageChoice]);*/
+
   k = atoi(ages[ageChoice]);
+
   cgiFormString("int",si,32);
   i = atoi(si);
   cgiFormString("ints",sl,32);
@@ -111,8 +243,6 @@ void RadioButtons(){
     m = j;
     fprintf(cgiOut, "a / b = %f",n / m);
   }
-	/*cgiFormString("age", ageText, 10);
-  fprintf(cgiOut, "Age of Truck: %s (method #2)<BR>\n", ageText);*/
 }
 void ShowForm()
 {
@@ -122,8 +252,17 @@ void ShowForm()
 	cgiValueEscape(cgiScriptName);
 	fprintf(cgiOut, "\">\n");
   fprintf(cgiOut, "<p>\n");
-  fprintf(cgiOut, "Your name<input type=\"text\" name=\"name\">\n");
+  fprintf(cgiOut, "ID<input type=\"text\" name=\"id\">\n");
+  fprintf(cgiOut, "Name<input type=\"text\" name=\"name\">\n");
+  fprintf(cgiOut, "Score<input type=\"text\" name=\"score\">\n");
+  fprintf(cgiOut, "<br>\n");
+  fprintf(cgiOut, "Delete ID<input type=\"text\" name=\"delete\">\n");
+  fprintf(cgiOut, "<br>\n");
+  fprintf(cgiOut, "Update ID<input type=\"text\" name=\"updateid\">\n");
+  fprintf(cgiOut, "Update score<input type=\"text\" name=\"updates\">\n");
   fprintf(cgiOut, "<br><br>\n");
+	fprintf(cgiOut, "<input type=\"checkbox\" name=\"show\" value=\"Show\" checked>\n");
+	fprintf(cgiOut, "<br>");
   fprintf(cgiOut, "a=:<input type=\"text\" name=\"int\">\n");
   fprintf(cgiOut, "<br>\n");
   fprintf(cgiOut, "<input type=\"radio\" name=\"age\" value=\"1\" checked>+\n");
